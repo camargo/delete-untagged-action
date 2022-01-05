da5101747af75bf07487ccca6e1785901c85f6f0
camargo <camargo.cac@gmail.com>
2022-01-04 18:37:49 -0800

    refactor: remove unused options

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function main() {
   try {
     const token = core.getInput('github-token', { required: true });
     const repository = core.getInput('repository', { required: true });
-    const github = getOctokit(token, opts);
+    const github = getOctokit(token);
 
     const [org, repo] = repository.split('/');
     const getUrl = `GET /orgs/${org}/packages/container/${repo}/versions`;
@@ -14,7 +14,7 @@ async function main() {
     for (const version of versions) {
       if (!version.metadata.container.tags.length) {
         const { id } = version;
-        console.log(`deleting version with id: ${id}`);
+        console.log(`deleting package version with id: ${id}`);
         const delUrl = `DELETE /orgs/${org}/packages/container/${repo}/versions/${id}`;
         const { status } = await github.request(delUrl);
         console.log(`deleted status: ${status}`);
